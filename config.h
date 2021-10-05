0cd7ae5932325d2bd5f1151e65c4f53987af3ecb
slimerPuncher <kr3xohd@outlook.com>
2021-10-05 15:23:16 +0200

    color fix

--- config.h
+++ config.h
@@ -3,27 +3,34 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int startwithgaps[]    = { 0 };	/* 1 means gaps are used by default, this can be customized for each tag */
-static const unsigned int gappx[]   = { 10 };   /* default gap between windows in pixels, this can be customized for each tag */
+static const int startwithgaps[]    = { 1 };	/* 1 means gaps are used by default, this can be customized for each tag */
+static const unsigned int gappx[]   = { 5 };   /* default gap between windows in pixels, this can be customized for each tag */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Code Pro:size=12", "Font Awesome 5 Free:size=12", "Noto Sans Mono:size=12" };
 static const char dmenufont[]       = "Source Code Pro:size=12";
-/*background color*/
-static const char col_gray1[]       = "#222222";
+/*background color*/                                    /*defaults*/
+static const char col_gray1[]       = "#222222";        /*222222*/
 /*inactive window border color*/
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#444444";        /*444444*/
 /*font color*/
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#bbbbbb";        /*bbbbbb*/
 /*current tag and current window font color*/
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#eeeeee";        /*eeeeee*/
 /*top bar second color and active window border color*/
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#128759";        /*005577*/
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* audio */
